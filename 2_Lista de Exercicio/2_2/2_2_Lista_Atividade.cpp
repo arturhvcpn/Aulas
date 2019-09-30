7f316ad8a576be932e171477556d3f033ed25901
Artur <arturcastropolonorte@gmail.com>
2019-09-30 18:57:54 -0300

    2 lista de exercicio

---
+++ 2_Lista de Exercicio/2_2/2_2_Lista_Atividade.cpp
@@ -0,0 +1,48 @@
+// EXERCICIO INCOMPLETO
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int maior_numero(const int array0[], const int array1[], const int array2[], int tamanho)
+{
+    
+    tamanho = 1;
+    for(int i = 0, i < tamanho, i++);
+    {
+        //array0
+        if(array0[i] > array1 && array0[i] > array2 )
+        {                    
+                cout << "o maior valor È so array0 "<< array0[i]<< endl;
+        }
+        //array1
+        if(array1[i] > array0 && array1[i] > array2 )
+        {                    
+                cout << "o maior valor È array1"<< array0[i]<< endl;
+        }
+        //array2
+        if(array2[i] > array0 && array2[i] > array1 )
+        {                    
+                cout << "o maior valor È array2 "<< array0[i]<< endl;
+        }
+        else
+        {
+            cout << "tem erro nessa bagaÁa"
+            
+        }
+        }
+}
+    
+void soma_array(const int array0[], const int array1[], const int array2[], tamanho)
+{
+    
+    }
+void selecionar_menor_array(const int array0[], const int array1[], const int array2[], tamanho)
+{
+    
+    }
+int main()
+{
+  
+}
